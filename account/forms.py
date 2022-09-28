6cca0a70223f747026b4774d03561a6169588876
elemasamuel <99813035+elemasamuel@users.noreply.github.com>
2022-09-28 12:05:44 +0100

    account registration added

---
+++ account/forms.py
@@ -0,0 +1,29 @@
+from dataclasses import fields
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+
+from account.models import Account
+
+class RegistrationForm(UserCreationForm):
+	email = forms.EmailField(max_length=254, help_text='Required. Add a valid email address.')
+
+	class Meta:
+		model = Account
+		fields = ('email', 'username', 'password1', 'password2', )
+
+	def clean_email(self):
+		email = self.cleaned_data['email'].lower()
+		try:
+			account = Account.objects.exclude(pk=self.instance.pk).get(email=email)
+		except Account.DoesNotExist:
+			return email
+		raise forms.ValidationError(f"Email {email} is already in use.")
+
+	def clean_username(self):
+		username = self.cleaned_data['username']
+		try:
+			account = Account.objects.exclude(pk=self.instance.pk).get(username=username)
+		except Account.DoesNotExist:
+			return username
+		raise forms.ValidationError(f"Username {username} is already in use.")
