68d056941c89b107b8bf9f7bf122258dee246dbb
elemasamuel <99813035+elemasamuel@users.noreply.github.com>
2022-09-29 09:23:06 +0100

    added login

--- ChatServerPlayground/urls.py
+++ ChatServerPlayground/urls.py
@@ -24,13 +24,17 @@ from personal.views import (
 
 from account.views import(
     register_view,
+    login_view,
+    logout_view
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view, name='home'),
 
-    path('register/', register_view, name='register')
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
 
 if settings.DEBUG:
--- account/forms.py
+++ account/forms.py
@@ -2,6 +2,7 @@ from dataclasses import fields
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
+from django.urls import is_valid_path
 
 from account.models import Account
 
@@ -27,3 +28,17 @@ class RegistrationForm(UserCreationForm):
 		except Account.DoesNotExist:
 			return username
 		raise forms.ValidationError(f"Username {username} is already in use.")
+
+class AccountAuthenticationForm(forms.ModelForm):
+	password = forms.CharField(label="Password", widget=forms.PasswordInput)
+
+	class Meta:
+		model = Account
+		fields = ("email", "password")
+
+	def clean(self):
+		if self.is_valid():
+			email = self.cleaned_data['email']
+			password = self.cleaned_data['password']
+			if not authenticate(email=email, password=password):
+				raise forms.ValidationError("Invalid Login")
