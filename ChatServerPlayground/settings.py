09ea51b1a029b1a6236b332b6e47e269cc279abe
elemasamuel <99813035+elemasamuel@users.noreply.github.com>
2022-10-01 19:32:15 +0100

    edit user account

--- ChatServerPlayground/settings.py
+++ ChatServerPlayground/settings.py
@@ -161,9 +161,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 TEMP = os.path.join(BASE_DIR, 'media_cdn/temp')
 
-BASE_URL = "http://127.0.0.1:8000"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BASE_URL = "http://127.0.0.1:8000"
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760 # 10 * 1024 * 1024 (10 MB)
